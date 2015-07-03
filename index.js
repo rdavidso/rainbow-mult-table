e9f7aecd24339481942232a0beca71a0bf8737d0
Ross Davidson <ross.e.davidson@gmail.com>
2015-07-03 18:00:50 -0500

    Adding option to render numbers as single unicode blocks, and non square sized multiplication tables.

--- index.js
+++ index.js
@@ -9,24 +9,25 @@ var colors = [  5, 4, 7, 8, 3, 9, 10, 11, 2, 12, 6, 13];
 var order = [0, 5, 7, 6, 8, 10, 9, 1, 0, 11];
 
 
-function mult(size, x, y, variablePadding) {
-    var sx = x + size;
-    var sy = y + size;
-    var pad = ("" + sx * sy).length;
-    var colPad = ("" + sx).length - 1;
+function mult(size, x, y, variablePadding, block, disableAxis) {
+    var sx = x + size[0];
+    var sy = y + size[1];
+    var pad = block ? ("" + sx).length : ("" + sx * sy).length;
+    pad = disableAxis ? 0 : pad;
+    var colPad = disableAxis ? 0 : ("" + sx).length - 1;
     var len = ("" + sy).length;
     for(var row = x; row <= sx; row++) {
         var line = "";
         for(var col = y; col <= sy; col++) {
             pad = variablePadding ? ("" + col * sx).length : pad;
             if(row !== x && col === y) {
-                line += padding(colPad, row);
+                line += padding(colPad, row, disableAxis);
             } else if (col !== y && row === x) {
-                line += padding(pad, col);
+                line += padding(pad, col, disableAxis);
             } else if (row === x && col === y) {
                 line += padding(colPad, ' ');
             } else {
-                line += padding(pad, row * col);
+                line += padding(pad, row * col, block);
             }
         }
         line += ansi.reset;
@@ -34,10 +35,11 @@ function mult(size, x, y, variablePadding) {
     }
 }
 
-function padding(pad, num) {
+function padding(pad, num, block) {
     var numStr = "" + num;
-    var len = numStr.length;
     var code = ansi.fg.codes[colors[order[parseInt(numStr[0])]]];
+    numStr = block ? "\u25ae" : numStr;
+    var len = numStr.length;
     var colorNumStr = code ? code + numStr : numStr;
     for(var i = 0; i <= pad - len; i++) {
         colorNumStr = " " + colorNumStr;
@@ -45,12 +47,23 @@ function padding(pad, num) {
     return colorNumStr;
 }
 
+function parseArr(str) {
+    var sp = str.split(',');
+    if(sp.length > 1) {
+        return [parseInt(sp[0]), parseInt(sp[1])];
+    } else {
+        return [parseInt(sp[0]), parseInt(sp[0])];
+    }
+}
+
 program
 .version('1.0.0')
-.option('-s, --size [n]', 'Print a multiplication table [n] * [n]', parseInt, 10)
+.option('-s, --size [n]', 'Print a multiplication table [n] * [n]', parseArr, [10, 10])
 .option('-r, --row-offset [offset]', 'Start your rows at 0 plus [offset]', parseInt, 0)
 .option('-c, --col-offset [offset]', 'Start your cols at 0 plus [offset]', parseInt, 0)
 .option('-p, --variable-padding', 'Enable minimal variable width padding.')
+.option('-b, --block', 'Render a colored block character instead of numbers')
+.option('-x, --disable-axis', 'Don\'t show the x,y axis')
 .parse(process.argv);
 
-mult(program.size, program.rowOffset, program.colOffset, program.variablePadding);
+mult(program.size, program.rowOffset, program.colOffset, program.variablePadding, program.block, program.disableAxis);
